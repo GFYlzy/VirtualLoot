185613b022f7767eb413c81564650a70ece5c964
GFYlzy <2435315247@qq.com>
2026-09-24 11:06:54 +0800

    Update BlockEntityTypeMixin.java

--- common/src/main/java/com/lunazstudios/virtualloot/mixin/BlockEntityTypeMixin.java
+++ common/src/main/java/com/lunazstudios/virtualloot/mixin/BlockEntityTypeMixin.java
@@ -1,5 +1,6 @@
 package com.lunazstudios.virtualloot.mixin;
 
+import com.lunazstudios.virtualloot.VirtualLoot; // 导入主类
 import com.cobblemon.mod.common.CobblemonBlockEntities;
 import com.lunazstudios.virtualloot.registry.VirtualLootBlocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -13,6 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class BlockEntityTypeMixin {
     @Inject(method = "isValid", at = @At("HEAD"), cancellable = true)
     private void virtualloot$acceptVirtualPasture(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+         // 如果 Cobblemon 尚未完全加载，直接跳过，避免触发类初始化
+        if (!VirtualLoot.cobblemonReady) {
+            return;
+        }
         if ((Object) this == CobblemonBlockEntities.PASTURE && VirtualLootBlocks.isVirtualPastureBlock(state.getBlock())) {
             cir.setReturnValue(true);
         }
